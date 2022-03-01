4b9fca81de931fc42c08de808a3f9cca7647093c
Gayane <gayane.mirakyan@ispras.ru>
2022-03-02 01:00:11 +0400

    Added signal handling homework

--- Homework_signal_handling/SignalHandling.cpp
+++ Homework_signal_handling/SignalHandling.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <sys/ucontext.h>
 #include <ucontext.h>
+#include <pwd.h>
 
 // function for signal handeling and changeing signal action
 void signal_handler(int signal_num, siginfo_t* siginfo, void* ucontext ) {
@@ -13,15 +14,23 @@ void signal_handler(int signal_num, siginfo_t* siginfo, void* ucontext ) {
     // cast void* to ucontext_t*
     struct ucontext_t* context = (ucontext_t*)ucontext;
 
+    pid_t pid = siginfo->si_pid;
+    uid_t uid = siginfo->si_uid;
+
     // print PID and UID
     std::cout << "signal is " << signal_num << std::endl 
-                << " - The PID of the sender " << siginfo->si_pid << std::endl 
-                << " - The UID of the sender " << siginfo->si_uid << std::endl;
+                << " - The PID of the sender " << pid << std::endl 
+                << " - The UID of the sender " << uid << std::endl;
+
+    // print user name
+    struct passwd *pw;
+    pw = getpwuid(uid);
+    std::cout << " - The user name of the sender " << pw->pw_name << std::endl;
 
     // print registers
-    std::cout << "REG_RIP =  " <<  context->uc_mcontext.gregs[REG_RIP]<< std::endl;
-    std::cout << "REG_RAX =  " <<  context->uc_mcontext.gregs[REG_RAX]<< std::endl;
-    std::cout << "REG_RBX =  " <<  context->uc_mcontext.gregs[REG_RBX]<< std::endl;
+    std::cout << " - REG_RIP =  " <<  context->uc_mcontext.gregs[REG_RIP]<< std::endl;
+    std::cout << " - REG_RAX =  " <<  context->uc_mcontext.gregs[REG_RAX]<< std::endl;
+    std::cout << " - REG_RBX =  " <<  context->uc_mcontext.gregs[REG_RBX]<< std::endl;
 
     exit(0);
 }
@@ -35,12 +44,14 @@ int main() {
 
     // I will write signal handler like this (*sa_sigaction)(int, siginfo_t *, void *)
     action.sa_sigaction = signal_handler;
+    action.sa_flags = SA_SIGINFO;
     sigaction(SIGUSR1, &action, NULL);
 
     // infinite loop
     while(true) {
         std::cout << "Infinite" << std::endl;
-        sleep(10);
+        sleep(1);
+        kill(getpid(), 10);
     }
     return 0;
 }
